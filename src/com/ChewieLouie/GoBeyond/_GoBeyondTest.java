139e5b7d8f6b2188fe4bda6cfc4944db1822bc50
Julian Churchill <chewielouie03@gmail.com>
2012-04-08 21:59:27 +0100

    Initial eclipse project structure

---
+++ src/com/ChewieLouie/GoBeyond/_GoBeyondTest.java
@@ -0,0 +1,14 @@
+package com.ChewieLouie.GoBeyond;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class _GoBeyondTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
