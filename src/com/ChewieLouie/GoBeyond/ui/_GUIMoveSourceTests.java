8182a188c2c116858f6946cf347e01a897cd6504
Julian Churchill <chewielouie03@gmail.com>
2012-05-01 21:20:19 +0100

    Converted some more tests to mockito

--- src/com/ChewieLouie/GoBeyond/ui/_GUIMoveSourceTests.java
+++ src/com/ChewieLouie/GoBeyond/ui/_GUIMoveSourceTests.java
@@ -1,5 +1,6 @@
 package com.ChewieLouie.GoBeyond.ui;
 
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -12,17 +13,17 @@ public class _GUIMoveSourceTests {
 
 	@Test
 	public void getMoveReturnsMoveBasedOnLastClickedPointOnBoardWidget() {
-		_TestableBoardWidget boardWidget = new _TestableBoardWidget();
-		boardWidget.getLastClickedBoardPointReturn = new Coord( 123, 543 );
+		BoardWidget boardWidget = mock( BoardWidget.class );
+		when( boardWidget.removeLastClickedBoardPoint() ).thenReturn( new Coord( 123, 543 ) );
 		GUIMoveSource g = new GUIMoveSource( boardWidget );
 
 		assertEquals( "last clicked point is used as next move",
-				boardWidget.getLastClickedBoardPointReturn, g.getMove( Move.Colour.Black, new SimpleBoard(9) ).coord() );
+				new Coord( 123, 543 ), g.getMove( Move.Colour.Black, new SimpleBoard(9) ).coord() );
 	}
 
 	@Test
 	public void enqueingAPassReturnsAPassOnNextGetMoveRequest() {
-		GUIMoveSource g = new GUIMoveSource( new _TestableBoardWidget() );
+		GUIMoveSource g = new GUIMoveSource( mock( BoardWidget.class ) );
 		g.enqueuePass();
 		assertEquals( "a pass is returned after it has been enqueued", 
 				Move.passMove(Move.Colour.Black), g.getMove(Move.Colour.Black, new SimpleBoard(9)) );
