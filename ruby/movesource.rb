bc1dbca8ad669c3d5fa18d7d0aca80143a500d60
Julian Churchill <chewielouie03@gmail.com>
2012-05-04 23:59:00 +0100

    Added basic unit test to movesource.rb to check pass move is returned

--- ruby/movesource.rb
+++ ruby/movesource.rb
@@ -1,7 +1,16 @@
+require 'test/unit'
 require 'java'
+$CLASSPATH << "C:\\Users\\Jennie\\git\\GoBeyond\\bin"
 
 Move = com.chewielouie.gobeyond.Move
 Coord = com.chewielouie.gobeyond.util.Coord
+
+class TestMoveSource < Test::Unit::TestCase
+    def testSourceReturnsAPass
+        source = MoveSource.new
+        assert_equal( source.getMove( Move::Colour::Black, nil ), Move.new( Coord.new( -1, -1 ), Move::Colour::Black ) )
+    end
+end
 
 class MoveSource 
 	include Java::com.chewielouie.gobeyond.MoveSource
