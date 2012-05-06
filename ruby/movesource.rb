0d4dd7c3af93e77fad96778a5a557a502a210339
Julian Churchill <chewielouie03@gmail.com>
2012-05-06 09:03:29 +0100

    Added tests to movesource.rb to make source not play on full eyes

--- ruby/movesource.rb
+++ ruby/movesource.rb
@@ -11,8 +11,8 @@ class TestMoveSource < Test::Unit::TestCase
   def setup
     @source = MoveSource.new
     @board = SimpleBoard::makeBoard('ww.'+
-                                    'bww'+
-                                    'bbw');
+                                    'ww.'+
+                                    '.ww');
   end
 
   def testSimpleBoardRespondsToEach
@@ -27,9 +27,17 @@ class TestMoveSource < Test::Unit::TestCase
   def testSourceReturnsOnlyEmptyPoints
     assert_equal( Move.new( Coord.new( 2, 0 ), Move::Colour::White ), @source.getMove( Move::Colour::White, @board ) )
     @board = SimpleBoard::makeBoard('www'+
-                                   'b.w'+
-                                   'bbw');
+                                    'w.w'+
+                                    '..w');
     assert_equal( Move.new( Coord.new( 1, 1 ), Move::Colour::White ), @source.getMove( Move::Colour::White, @board ) )
+  end
+
+  def testSourceDoesNotFillInFullEyes
+    @board = SimpleBoard::makeBoard('.www'+
+                                    'w.ww'+
+                                    'www.'+
+                                    'wwww');
+    assert_equal( Move::passMove( Move::Colour::White ), @source.getMove( Move::Colour::White, @board ) )
   end
 end
 
@@ -67,7 +75,43 @@ class MoveSource
     include Java::com.chewielouie.gobeyond.MoveSource
   def getMove(colour, board)
     if board.respond_to?("first_empty_point")
-      return Move.new( board.first_empty_point, colour )
+      move = Move.new( board.first_empty_point, colour )
+      return move if not isEye( move, board )
     end
+    return Move::passMove( colour )
+  end
+
+  def isEye( move, board )
+    friendlyBoardPoint = Board::Point::BlackStone;
+    if move.colour == Move::Colour::White
+      friendlyBoardPoint = Board::Point::WhiteStone;
+    end
+    return false if not allLibertiesOfPointAreFriendly( move.coord(), friendlyBoardPoint, board )
+    return countFriendlyPoints( move.coord(), friendlyBoardPoint, board ) >= 7
+  end
+
+  def allLibertiesOfPointAreFriendly( coord, friendlyBoardPoint, board )
+    return false if not isFriendlyPoint( coord.up(), friendlyBoardPoint, board )
+    return false if not isFriendlyPoint( coord.down(), friendlyBoardPoint, board )
+    return false if not isFriendlyPoint( coord.left(), friendlyBoardPoint, board )
+    return false if not isFriendlyPoint( coord.right(), friendlyBoardPoint, board )
+    return true
+  end
+
+  def isFriendlyPoint( coord, colour, board )
+    return (board.getContentsOfPoint( coord ) == Board::Point::OffBoard or board.getContentsOfPoint( coord ) == colour)
+  end
+
+  def countFriendlyPoints( coord, colour, board )
+    count = 0
+    count = count + 1 if isFriendlyPoint( coord.up(), colour, board )
+    count = count + 1 if isFriendlyPoint( coord.upLeft(), colour, board )
+    count = count + 1 if isFriendlyPoint( coord.upRight(), colour, board )
+    count = count + 1 if isFriendlyPoint( coord.down(), colour, board )
+    count = count + 1 if isFriendlyPoint( coord.downLeft(), colour, board )
+    count = count + 1 if isFriendlyPoint( coord.downRight(), colour, board )
+    count = count + 1 if isFriendlyPoint( coord.left(), colour, board )
+    count = count + 1 if isFriendlyPoint( coord.right(), colour, board )
+    return count
   end
 end
